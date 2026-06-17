fcbfe7048b9597365089ca06fa776d832dabdd23
xbrzz63x <xbrzz63x@gmail.com>
2026-06-17 21:12:08 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'cinescope-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install : mise en cache des fichiers
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate : nettoyage des anciens caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch : réseau d'abord, cache en fallback
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
